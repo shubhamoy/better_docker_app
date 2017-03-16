0cf1d4b9eaf8aba72606aba43bf7db068d3a2389
Shubhamoy Chakrabarty <me@shubhamoy.com>
2017-03-16 10:50:17 +0530

    Simple data emitter using Redis

---
+++ backend/app.js
@@ -0,0 +1,26 @@
+"use strict";
+let NRP = require("node-redis-pubsub");
+let redis_host = process.env.REDIS_HOST || "127.0.0.1";
+
+let config = {
+  port: 6379,
+	host: redis_host,
+	scope: "demo"
+};
+let server = new NRP(config);
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+setInterval(() => {
+  let recData = getRandomInt(50, 100);
+	let tataData = getRandomInt(75, 150);
+	let gailData = getRandomInt(25, 125);
+
+	server.emit("tickerData", [{rdata: recData},
+                             {tdata: tataData},
+                             {gdata: gailData}]
+	);
+}, 2000);
+console.log("Backend Server Running");
